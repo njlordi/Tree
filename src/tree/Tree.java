ef1d2c6a6e78d273c6a9b7fdc9c4b5700fda19ba
Nicholas Lordi <njlordi@gmail.com>
2017-03-06 19:34:30 -0500

    fixed AddTreeNode function so the tree is symmetrical.

--- src/tree/Tree.java
+++ src/tree/Tree.java
@@ -4,9 +4,16 @@ package tree;
 public class Tree {
 
     public static void main(String[] args) {
-        TreeNode myTree = new TreeNode(0);
-        myTree.AddTreeNode(1);
+        TreeNode myTree = new TreeNode(1);
         myTree.AddTreeNode(2);
+        myTree.AddTreeNode(3);
+        myTree.AddTreeNode(4);
+        myTree.AddTreeNode(5);
+        myTree.AddTreeNode(6);
+        myTree.AddTreeNode(7);
+        myTree.AddTreeNode(8);
+        myTree.AddTreeNode(9);
+        myTree.AddTreeNode(10);
         myTree.PrintTree();
     }
     
--- src/tree/TreeNode.java
+++ src/tree/TreeNode.java
@@ -6,14 +6,12 @@ public class TreeNode {
 
     public int someValue;
     public static int sizeOfTree;
-    public TreeNode parent;
     public TreeNode leftChild;
     public TreeNode rightChild;
 
     // Constructor for root
     public TreeNode(int someValue) {
         this.someValue = someValue;
-        parent = null;
         leftChild = null;
         rightChild = null;
         sizeOfTree = 1;
@@ -22,29 +20,36 @@ public class TreeNode {
     // Contructor for child
     public TreeNode(int someValue, TreeNode a, TreeNode b) {
         this.someValue = someValue;
-        parent = null;
         leftChild = a;
         rightChild = b;
-        sizeOfTree++;
     }
     
     public void AddTreeNode(int newValue) {
-        
         if (leftChild == null) {
             leftChild = new TreeNode(newValue, null, null);
+            sizeOfTree++;
         } else if (rightChild == null) {
             rightChild = new TreeNode(newValue, null, null);
+            sizeOfTree++;
+        } else if (sizeOfTree % 2 == 0) {
+            leftChild.AddTreeNode(newValue);
+        } else {
+            rightChild.AddTreeNode(newValue);
         }
     }
 
     public void PrintTree() {
-        System.out.println("Value is: " + this.someValue + " Size of tree is: " + sizeOfTree);
+        
+        System.out.println(this.someValue);
+        
         if (leftChild != null) {
             leftChild.PrintTree();
-        }
+        } 
+        
         if (rightChild != null){
             rightChild.PrintTree();
         }
+        
     }
 
 }
